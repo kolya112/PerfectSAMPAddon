0114199f8f14c6ff99568afc67b7d7f099dc2d89
kolya112 <nikolayurch@gmail.com>
2021-12-22 12:51:51 +0300

    Добавьте файлы проекта.

---
+++ PerfectSAMPPatch/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace PerfectSAMPPatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.getSAMPscreens = new System.Windows.Forms.Button();
+            this.getgameclient = new System.Windows.Forms.Button();
+            this.settings = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(235, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to SAMP 0.3.7 starter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(631, 389);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 52);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Powered by Николай Юрченко\r\naka Sharp_Macalister,\r\nNikolay_Aweiro\r\nNikolay_Mason";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(13, 77);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(267, 88);
+            this.start.TabIndex = 2;
+            this.start.Text = "Играть";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // getSAMPscreens
+            // 
+            this.getSAMPscreens.Location = new System.Drawing.Point(13, 172);
+            this.getSAMPscreens.Name = "getSAMPscreens";
+            this.getSAMPscreens.Size = new System.Drawing.Size(267, 48);
+            this.getSAMPscreens.TabIndex = 3;
+            this.getSAMPscreens.Text = "Открыть папку с скриншотами SAMP";
+            this.getSAMPscreens.UseVisualStyleBackColor = true;
+            this.getSAMPscreens.Click += new System.EventHandler(this.getSAMPscreens_Click);
+            // 
+            // getgameclient
+            // 
+            this.getgameclient.Location = new System.Drawing.Point(13, 227);
+            this.getgameclient.Name = "getgameclient";
+            this.getgameclient.Size = new System.Drawing.Size(267, 45);
+            this.getgameclient.TabIndex = 4;
+            this.getgameclient.Text = "Открыть папку с клиентом игры";
+            this.getgameclient.UseVisualStyleBackColor = true;
+            this.getgameclient.Click += new System.EventHandler(this.getgameclient_Click);
+            // 
+            // settings
+            // 
+            this.settings.Location = new System.Drawing.Point(13, 279);
+            this.settings.Name = "settings";
+            this.settings.Size = new System.Drawing.Size(267, 53);
+            this.settings.TabIndex = 5;
+            this.settings.Text = "Настройки";
+            this.settings.UseVisualStyleBackColor = true;
+            this.settings.Click += new System.EventHandler(this.settings_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.settings);
+            this.Controls.Add(this.getgameclient);
+            this.Controls.Add(this.getSAMPscreens);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "SAMPLauncher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button getSAMPscreens;
+        private System.Windows.Forms.Button getgameclient;
+        private System.Windows.Forms.Button settings;
+    }
+}
+
---
+++ PerfectSAMPPatch/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Threading;
+using INIManager;
+using UnkovSAMPAPI;
+
+namespace PerfectSAMPPatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HubManager settings = new HubManager(Application.StartupPath + @"\common\settings.ini"); // Инициализируем переменную settings для управления файлом settings.ini
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            HubManager settings1 = new HubManager(Application.StartupPath + @"\common\settings.ini"); // Инициализируем переменную settings для управления файлом settings.ini
+            string mypath = settings1.GetString("main", "path"); // Получение из настроек путь к samp.exe
+            string adress = settings1.GetString("main", "adress"); // Получение из настроек IP:порт
+            string nick = settings1.GetString("main", "nick"); // Получение из настроек ника
+            using (RegistryKey checknick = Registry.CurrentUser.OpenSubKey(@"Software\SAMP", true)) // Инициализация реестра с разделом SAMP
+            {
+                if (checknick != null) // Проверка существование на раздел SAMP в реестре
+                {
+                    if (checknick.GetValue("PlayerName") != null) // Проверка на существования PlayerName переменной в реестре, разделе SAMP
+                    {
+                        checknick.SetValue("PlayerName", nick); // Приравниваем переменную nick из settings.ini в реестр, раздел SAMP
+                    }
+                }
+            }
+            //MessageBox.Show("Debug code: 1");
+            Process.Start(mypath, adress); // Запускаем игру
+            Thread.Sleep(30000); // Программа засыпает на 30 секунд
+
+            var runningProcs = from proc in Process.GetProcesses(".") orderby proc.Id select proc;
+            if (runningProcs.Count(p => p.ProcessName.Contains("gta_sa")) > 0) // Проверяем, запущен ли процесс GTA SA на компьютере
+            {
+                // Запускаем патчи для SAMP
+                Thread one = new Thread(one1);
+                //one.Start();
+                Process process = Process.GetProcessesByName("gta_sa")[0];
+                IntPtr processHandle = SAMPAPI.OpenProcess(0x1F0FFF, false, process.Id);
+
+                //int bytesWritten = 0;
+                int bytesReading = 0;
+                //byte[] buffer = Encoding.Unicode.GetBytes("It works!\0");
+                byte[] buffer = new byte[30]; //'Hello World!' takes 12*2 bytes because of Unicode
+                // '\0' marks the end of string
+
+                // replace 0x0046A3B8 with your address
+                //SAMPAPI.WriteProcessMemory((int)processHandle, 0x0046A3B8, buffer, buffer.Length, ref bytesWritten);
+                SAMPAPI.ReadProcessMemory((int)processHandle, 0xB7CE50, buffer, buffer.Length, ref bytesReading);
+                API.AddChatMessage(Encoding.Unicode.GetString(buffer) + "(" + bytesReading.ToString() + "bytes)");
+                /*Thread two = new Thread(two2);
+                two.Start();
+                Thread three = new Thread(three3);
+                three.Start();
+                Thread four = new Thread(four4);
+                four.Start();
+                Thread five = new Thread(five5);
+                five.Start();*/
+            }
+            else
+            {
+
+            }
+        }
+
+        private void getSAMPscreens_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void getgameclient_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void settings_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void one1()
+        {
+            do
+            {
+                if (API.GetFrameRate() < 10)
+                {
+                    
+                }
+                API.AddChatMessage("[SAMPPatch] Система обнаружила, что у Вас очень маленький FPS, закройте другие процессы.");
+                if (API.GetFrameRate() > 10)
+                {
+
+                }
+            }
+            while (true);
+        }
+
+        public static void two2()
+        {
+            do
+            {
+
+            }
+            while (true);
+        }
+
+        public static void three3()
+        {
+            do
+            {
+
+            }
+            while (true);
+        }
+
+        public static void four4()
+        {
+            do
+            {
+
+            }
+            while (true);
+        }
+
+        public static void five5()
+        {
+            do
+            {
+
+            }
+            while (true);
+        }
+    }
+}
